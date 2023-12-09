f678e4f40270e65b3d9def1ce53e8abfe0682d2d
Dominik Lewczyński <dominiklew200060@gmail.com>
2023-12-09 19:19:27 +0100

    Update urls.py by add new path to download pdf file

--- text_analysis/urls.py
+++ text_analysis/urls.py
@@ -1,7 +1,8 @@
 # text_analysis/urls.py
 from django.urls import path
-from .views import home
+from .views import home, download_report
 
 urlpatterns = [
     path('', home, name='home'),
+    path('download/<path:url>/', download_report, name='download_report'),
 ]
