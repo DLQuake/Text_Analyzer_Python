ac783b3ac258d7bbf63bf2cc979cd95b26abd1f8
Dominik Lewczyński <dominiklew200060@gmail.com>
2023-12-16 18:52:12 +0100

    Import new functions from utils.py and update function home

--- text_analysis/views.py
+++ text_analysis/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import URLForm
-from .utils import get_text_from_url, analyze_text, analyze_sentiment, extract_keywords, save_report_to_pdf
+from .utils import get_text_from_url
+from .utils import analyze_text
+from .utils import analyze_sentiment
+from .utils import extract_keywords
+from .utils import generate_pdf_report
+from .utils import generate_summary
+from .utils import detect_fake_news
 
 
 def home(request):
@@ -13,8 +19,24 @@ def home(request):
             word_freq = analyze_text(text)
             sentiment = analyze_sentiment(text)
             keywords = extract_keywords(text)
-            save_report_to_pdf(url_to_analyze, word_freq, sentiment, keywords)
-            return render(request, 'text_analysis/results.html', {'top_words': word_freq.most_common(10), 'url_to_analyze': url_to_analyze, 'sentiment': sentiment, 'keywords': keywords})
+            summary = generate_summary(text)
+            fake_news_status = detect_fake_news(summary)
+
+            # Zapisz raport w formie pliku PDF
+            generate_pdf_report(url_to_analyze, word_freq, sentiment, keywords, summary, fake_news_status)
+
+            # Przygotuj dane do przekazania do szablonu HTML
+            context = {
+                'top_words': word_freq.most_common(10),
+                'url_to_analyze': url_to_analyze,
+                'sentiment': sentiment,
+                'keywords': keywords,
+                'summary': summary,
+                'fake_news_status': fake_news_status
+            }
+
+            # Renderuj szablon HTML
+            return render(request, 'text_analysis/results.html', context)
     else:
         form = URLForm()
 
@@ -24,6 +46,7 @@ def home(request):
 def download_report(request, url):
     file_path = f"raporty/raport_z_{url.replace('http://', '').replace('https://', '').replace('/', '_')}.pdf"
     with open(file_path, 'rb') as pdf_file:
-        response = HttpResponse(pdf_file.read(), content_type='application/pdf')
+        response = HttpResponse(
+            pdf_file.read(), content_type='application/pdf')
         response['Content-Disposition'] = f'attachment; filename=raport_{url.replace("http://", "").replace("https://", "").replace("/", "_")}.pdf'
         return response
