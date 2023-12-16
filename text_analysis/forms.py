de40db75c2fc2d1e6e1c0a60435d1d88b1c0a86b
Dominik Lewczyński <dominiklew200060@gmail.com>
2023-12-16 18:48:59 +0100

    Update label in forms.py

--- text_analysis/forms.py
+++ text_analysis/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 
 class URLForm(forms.Form):
-    url_to_analyze = forms.URLField(label='Wprowadź link do analizy')
+    url_to_analyze = forms.URLField(label='Link do analizy')
